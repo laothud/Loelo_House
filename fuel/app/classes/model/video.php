80475d9f63eb2f9148006a09e2ba09e63bb0b1fe
Chris Henry <laothud@yahoo.com>
2014-05-01 20:22:34 -0400

    created art and video basic models
    Need to expand the models for CRUD

---
+++ fuel/app/classes/model/video.php
@@ -0,0 +1,32 @@
+<?php
+
+class Model_Art extends Orm\Model
+{
+	/**
+	 *
+	 */
+	protected static $_properties = array(
+		'id',
+		'videocode',
+		'userid', 
+		'created_at',
+		'username',
+    );
+
+    protected static $_observers = array(
+	    'Orm\\Observer_CreatedAt' => array(
+	        'events' => array('before_insert'),
+	        'mysql_timestamp' => true,
+	    ),
+	);
+
+    public static function get_results()
+    {
+		return static::query()->order_by('created_at', 'desc')->get();
+    }
+
+    public static function get_one()
+    {
+    	return static::query()->order_by('created_at', 'desc')->get_one();
+    }
+}
