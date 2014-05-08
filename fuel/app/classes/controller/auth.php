d51d2cff330a4f5675645476b05ae000e3417481
Chris Henry <laothud@yahoo.com>
2014-05-08 16:43:38 -0400

    Readjusting the auth controller
    Updated to ormauth instead of hand built.

--- fuel/app/classes/controller/auth.php
+++ fuel/app/classes/controller/auth.php
@@ -19,49 +19,32 @@ class Controller_Auth extends Controller_App
 	 */
 	public function post_login()
 	{
-		if (Auth::check())
+		// check to see if they are already logged in.
+	    if (Auth::check())
 	    {
-	        Response::redirect('/');
+	      	//redirect to dash of said user group
+	      	if () {
+	      		Response::redirect('admin_dash');
+	      	}
+	        
+	        Response::redirect('user_dash');
 	    }
 
-		$val = Validation::forge('my_validation');
-		$val->add_field('username', 'Your_username', 'required');
-		$val->add_field('password', 'Your_password', 'required|min_length[3]|max_length[10]');
-
-		if (! $val->run())
-		{
-			Response::redirect('login');
-		}
-
-		// Auth::login();
-
-		$user = Model_User::get_one($username);
-		$userList = [];
-
-		if ($user != null) 
-		{
-			if ($username == "Admin" && $user->name == "Admin"){
-    			if (md5($password) == $user->password)
-    			{
-    				$this->template->content = View::forge('site/admin_dash');
-    				$this->template->content->posts = Model_Post::get_results();
-    			}
-    		}
-    		else if ($username == $user->name) 
-    		{
-    			if(md5($password) == $user->password)
-    			{
-    				$this->template->content = View::forge('site/user_dash');
-    				$this->template->content->posts = Model_Post::get_results();
-    			}
-    		}
-    		else
-    		{
-    			Response::redirect('login');
-    		}
-	    	
-		}	
-		
+	    // was the login form posted?
+	    if (Input::method() == 'POST')
+	    {
+	        // check the credentials.
+	        if (Auth::instance()->login(Input::param('username'), Input::param('password')))
+	        {
+	            // logged in, go to the appropriate dash
+	            Response::redirect('admin_dash');
+	        }
+	        else
+	        {
+	            // login failed, show error
+	            Messages::error(__('login.failure'));
+	        }
+	    }
 	}
 
 }
